b14d48f35153026a696fc1430325de38eee3b48b
dyxweb <13483409091@163.com>
2023-09-09 15:55:00 +0800

    feat: 添加打包分析配置

---
+++ config/webpack.analysis.js
@@ -0,0 +1,11 @@
+const { merge } = require('webpack-merge');
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const prodConfig = require('./webpack.prod.js');
+const smp = new SpeedMeasurePlugin();
+
+module.exports = smp.wrap(merge(prodConfig, {
+  plugins: [
+    new BundleAnalyzerPlugin() // 配置分析打包结果插件
+  ]
+}))

6b2dc5e17cb17ad18afbee8ee042630b00bd90a1
dyxweb <13483409091@163.com>
2023-09-09 15:31:10 +0800

    feat: 压缩css文件

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,9 +1,15 @@
 const { merge } = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
 const commonConfig = require('./webpack.common.js');
 
 module.exports = merge(commonConfig, {
   mode: 'production', // 生产模式
+  optimization: {
+    minimizer: [
+      new CssMinimizerWebpackPlugin(), // 压缩css
+    ],
+  },
   plugins: [
     // 抽离css插件
     new MiniCssExtractPlugin({

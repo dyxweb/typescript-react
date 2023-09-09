162cca8770e3fdd373b51bf98d3251fe41c7465d
dyxweb <13483409091@163.com>
2023-09-09 15:40:37 +0800

    feat: 开启持久化存储缓存

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -19,6 +19,9 @@ module.exports = {
     // 默认是.js和.json。以下配置解决ts文件无法被引用解析的问题
     extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
   },
+  cache: {
+    type: 'filesystem', // 使用文件缓存
+  },
   module: {
     rules: [
       {
